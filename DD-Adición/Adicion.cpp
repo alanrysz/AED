6c33131cb9481165793ce545024591ecc0d6942e
alanrysz <alandvc@gmail.com>
2019-04-25 00:33:01 -0300

    Update Adicion.cpp

--- DD-Adición/Adicion.cpp
+++ DD-Adición/Adicion.cpp
@@ -10,8 +10,7 @@ int main(){
 
 	double a, b;
 
-	cout << "Escribe 2 numeros para luego realizar la suma";
-	cin >>a >> b;
+	cin >> a >> b;
 	cout << "La suma de " << a << "+ " << b << "es: " << a+b;
 
 	return 0;
