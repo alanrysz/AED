34d4f8b5d35526a7cf0ee75a4b5ac142fce5b436
alanrysz <alandvc@gmail.com>
2019-04-25 00:28:33 -0300

    Create Adicion.cpp

---
+++ DD-Adición/Adicion.cpp
@@ -0,0 +1,18 @@
+/*	Adición
+	AMR
+	20192404
+*/
+
+#include <iostream>
+namespace std;
+
+int main(){
+
+	double a, b;
+
+	cout << "Escribe 2 numeros para luego realizar la suma";
+	cin >>a >> b;
+	cout << "La suma de " << a << "+ " << b << "es: " << a+b;
+
+	return 0;
+}
